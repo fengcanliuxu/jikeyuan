638a11020fc5afc140f085745c769f34d0f398ab
fengcanliuxu <1944862639@qq.com>
2022-06-05 18:16:25 +0800

    项目搭建配置路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Home from './pages/Layout'
+import Login from './pages/Login'
 function App() {
-  return <div>App</div>
+  return (
+    <Router>
+      <div className="App">
+        {/* <Link to="/login">登录</Link>
+        <Link to="./home">首页</Link> */}
+      </div>
+      <Switch>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/login" component={Login}></Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/Layout/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return <div>首页布局组件</div>
+  }
+}
