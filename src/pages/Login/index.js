4d7d99be4f7f548f16491546a43812affad00e1c
fengcanliuxu <1944862639@qq.com>
2022-06-07 08:08:12 +0800

    登录基本结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Card } from 'antd'
+import './index.css'
+import logo from '../../assets/logo.png'
 export default class index extends Component {
   render() {
     return (
-      <div>
-        登录组件<Button type="primary">按钮</Button>
+      <div className="login">
+        <Card className="login-container">
+          <img src={logo} alt="" className="login-logo" />
+        </Card>
       </div>
     )
   }
